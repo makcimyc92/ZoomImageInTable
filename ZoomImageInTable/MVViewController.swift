4d7731a809c2a90db6affce1c267721591d03dab
makcimyc92 <makcimyc92@gmail.com>
2017-03-30 20:39:18 +0300

    Change zooming image

--- ZoomImageInTable/MVViewController.swift
+++ ZoomImageInTable/MVViewController.swift
@@ -15,7 +15,6 @@ class MVViewController: UIViewController,UITableViewDataSource {
                            "https://s-media-cache-ak0.pinimg.com/originals/b4/77/23/b4772340ae9c206149a0a7338767c530.jpg",
                            "https://upload.wikimedia.org/wikipedia/commons/5/5e/Big_Wood%2C_N2.JPG",
                            "https://upload.wikimedia.org/wikipedia/commons/d/d9/Big_Bear_Valley,_California.jpg"]
-    
     let imageCache = NSCache<AnyObject, UIImage>()
 
     override func viewDidLoad() {
@@ -28,6 +27,36 @@ class MVViewController: UIViewController,UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    private var zoomImageView:UIImageView?
+    func handlerPinchGesture(_ sender: UIPinchGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            let location = sender.location(in: sender.view!)
+            self.zoomImageView = UIImageView.init(image: (sender.view as! UIImageView).image)
+            self.zoomImageView?.layer.anchorPoint = CGPoint(x: location.x / sender.view!.bounds.width,
+                                                            y: location.y / sender.view!.bounds.height)
+            self.zoomImageView?.contentMode = .scaleAspectFit
+            self.zoomImageView?.frame = self.view.convert(sender.view!.bounds, from: sender.view!)
+            self.view.addSubview(self.zoomImageView!)
+            sender.view?.isHidden = true
+        case .changed:
+            self.zoomImageView!.transform = .init(scaleX: sender.scale, y: sender.scale)
+        case .ended:
+            sender.view!.isHidden = false
+            sender.view!.alpha = 0;
+            UIView.animate(withDuration: 0.3, animations: {
+                sender.view!.alpha = 1;
+                self.zoomImageView?.alpha = 0
+                self.zoomImageView!.transform = CGAffineTransform.identity
+            }, completion: { (finished) in
+                self.zoomImageView?.removeFromSuperview()
+                self.zoomImageView = nil
+            })
+        default:
+            break
+        }
+    }
+    
     //MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,10 +66,12 @@ class MVViewController: UIViewController,UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ZoomImageCell") as! MVZoomImageCell
         let urlString = self.imageDataArray[indexPath.row]
+        let pinch = UIPinchGestureRecognizer.init(target: self, action: #selector(handlerPinchGesture(_:)))
+        cell.bigImage.addGestureRecognizer(pinch)
         if let image = self.imageCache.object(forKey:indexPath as AnyObject) {
-            cell.bigImage?.image = image
+            cell.bigImage.image = image
         }else{
-            cell.bigImage?.image = nil
+            cell.bigImage.image = nil
             URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
                 
                 if error != nil {
--- ZoomImageInTable/MVZoomImageView.swift
+++ ZoomImageInTable/MVZoomImageView.swift
@@ -9,34 +9,9 @@
 import UIKit
 
 class MVZoomImageView: UIImageView {
-    
-    var startFrame: CGRect = CGRect.zero
 
     override func awakeFromNib() {
         super.awakeFromNib()
         self.isUserInteractionEnabled = true
-        self.addGestureToSelf()
     }
-    
-    func addGestureToSelf() {
-        let pinch = UIPinchGestureRecognizer.init(target: self, action: #selector(handlerPinchGesture(_:)))
-        self.addGestureRecognizer(pinch)
-    }
-    
-    func handlerPinchGesture(_ sender: UIPinchGestureRecognizer) {
-        if self.image == nil {
-            return
-        }
-        switch sender.state {
-        case .changed:
-            self.transform = CGAffineTransform.init(scaleX: sender.scale, y: sender.scale)
-        case .ended:
-            UIView.animate(withDuration: 0.2) {
-                self.transform = CGAffineTransform.identity
-            }
-        default:
-            break
-        }
-    }
-
 }
